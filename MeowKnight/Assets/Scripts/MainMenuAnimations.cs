9cd992f598102efe26809f9e67e6796a293de653
April Hussey <april.hussey@outlook.com>
2023-04-30 20:23:39 +0100

    Added animations for when credits and quit are pressed

--- MeowKnight/Assets/Scripts/MainMenuAnimations.cs
+++ MeowKnight/Assets/Scripts/MainMenuAnimations.cs
@@ -9,5 +9,16 @@ public class MainMenuAnimations : MonoBehaviour
     public void StartPressed()
     {
         mainMenuAnimator.SetBool("startPressed", true);
-    } 
+    }
+
+	public void CreditsPressed()
+	{
+        transform.Rotate(0, 180, 0);
+        mainMenuAnimator.SetBool("creditsPressed", true);
+	}
+
+	public void QuitPressed()
+    {
+        mainMenuAnimator.SetBool("quitPressed", true);
+    }
 }
