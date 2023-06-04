1e62d28e7fd1c57b810a60a5fff7f3aec2c44ea9
April Hussey <april.hussey@outlook.com>
2023-06-04 12:34:30 +0100

    Hm

--- MeowKnight/Assets/Scripts/PlayerController.cs
+++ MeowKnight/Assets/Scripts/PlayerController.cs
@@ -7,44 +7,44 @@ using UnityEngine.Events;
 public class PlayerController : MonoBehaviour
 {
 
-    public float moveSpeed = 5f;
+	public float moveSpeed = 5f;
 
-    public float minJumpForce = 1f;   // Min jump force
+	public float minJumpForce = 1f;   // Min jump force
 	public float maxJumpForce = 8f;   // Max jump force
-    public float jumpForceIncrement = .1f;   // Force increment
+	public float jumpForceIncrement = .1f;   // Force increment
 	public float currentJumpForce;    // Current amount of force added when the player jumps
 	private bool jumping = false;
 
-    public float bounceForce = 100f;
-    public LayerMask wallLayer;
+	public float bounceForce = 100f;
+	public LayerMask wallLayer;
 
-    private Rigidbody2D rigidbody;
-    private Vector2 moveDirection;
-    public InputActions inputActions;
+	private Rigidbody2D rigidbody;
+	private Vector2 moveDirection;
+	public InputActions inputActions;
 
-    [SerializeField] private LayerMask whatIsGround;    // A mask determining what is ground to the player
-    [SerializeField] private Transform groundCheck; // A position marking where to check if the player is grounded
-    [SerializeField] private Transform ceilingCheck;    // A position marking where to check for ceilings
+	[SerializeField] private LayerMask whatIsGround;    // A mask determining what is ground to the player
+	[SerializeField] private Transform groundCheck; // A position marking where to check if the player is grounded
+	[SerializeField] private Transform ceilingCheck;    // A position marking where to check for ceilings
 
-    const float groundedRadius = .5f;   // Radius of the overlap circle to determine if grounded
+	const float groundedRadius = .5f;   // Radius of the overlap circle to determine if grounded
 	private bool grounded;  // Whether or not the player is grounded
 	const float ceilingRadius = .5f;    // Radius of the overlap circle to determine if the player has hit a ceiling
-    private bool facingRight = true;    // For determining which way the player is currently facing
+	private bool facingRight = true;    // For determining which way the player is currently facing
 
-    public UnityEvent onLandEvent;
+	public UnityEvent onLandEvent;
 
 	void Awake()
-    {
-        inputActions = new InputActions();
-        inputActions.Player.Movement.performed += context => moveDirection = context.ReadValue<Vector2>();
-        inputActions.Player.Movement.canceled += context => moveDirection = Vector2.zero;
+	{
+		inputActions = new InputActions();
+		inputActions.Player.Movement.performed += context => moveDirection = context.ReadValue<Vector2>();
+		inputActions.Player.Movement.canceled += context => moveDirection = Vector2.zero;
 
 		rigidbody = GetComponent<Rigidbody2D>();
 
 		if (onLandEvent == null)
-        {
-            onLandEvent = new UnityEvent();
-        }
+		{
+			onLandEvent = new UnityEvent();
+		}
 	}
 
 	void OnEnable()
@@ -59,9 +59,9 @@ public class PlayerController : MonoBehaviour
 
 	// Update is called once per frame
 	void FixedUpdate()
-    {
-        bool wasGrounded = grounded;
-        grounded = false;
+	{
+		bool wasGrounded = grounded;
+		grounded = false;
 
 		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
 		Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundedRadius, whatIsGround);
@@ -70,109 +70,113 @@ public class PlayerController : MonoBehaviour
 			if (colliders[i].gameObject != gameObject)
 			{
 				grounded = true;
-                if (!wasGrounded)
-                {
-                    onLandEvent.Invoke();
-                }
+				if (!wasGrounded)
+				{
+					onLandEvent.Invoke();
+				}
 			}
 		}
-
-        Move(moveDirection.x);
+		if (grounded && !jumping)
+		{
+			Move(moveDirection.x);
+		}
 	}
 
-    void Update()
-    {
-        Debug.Log("jumping = " + jumping);
+	void Update()
+	{
+		Debug.Log("jumping = " + jumping);
 		Debug.Log("grounded = " + grounded);
 		if (grounded)
-        {
-            if (jumping)
-            {
-                currentJumpForce += jumpForceIncrement * Time.deltaTime;
+		{
+			if (jumping)
+			{
+				currentJumpForce += jumpForceIncrement * Time.deltaTime;
 
-                if (currentJumpForce >= maxJumpForce)
-                {
-                    Jump();
-                }
-            }
-        }
+				if (currentJumpForce >= maxJumpForce)
+				{
+					Jump();
+				}
+			}
+		}
 	}
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Wall"))
-        {
-            // Calculate the direction to bounce in
-            Vector2 bounceDirection = collision.contacts[0].normal;
-            // Apply the bounce force
-            rigidbody.AddForce(bounceDirection * bounceForce, ForceMode2D.Impulse);
-        }
-    }
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.layer == LayerMask.NameToLayer("Wall"))
+		{
+			// Calculate the direction to bounce in
+			Vector2 bounceDirection = collision.contacts[0].normal;
+			// Apply the bounce force
+			rigidbody.AddForce(bounceDirection * bounceForce, ForceMode2D.Impulse);
+		}
+	}
 
-    void Move(float move)
-    {
-        if (grounded && !jumping)
-        {
-            // Move the player using velocity
-            rigidbody.velocity = new Vector2(moveDirection.x * moveSpeed, rigidbody.velocity.y);
+	void Move(float move)
+	{
+		if (grounded && !jumping)
+		{ 
+			// Move the player using velocity
+			rigidbody.velocity = new Vector2(moveDirection.x * moveSpeed, rigidbody.velocity.y);
 
 			// If the input is moving the player right and the player is facing left...
 			if (move > 0 && !facingRight)
-            {
-                // ... flip the player
-                Flip();
-            }
-			// Otherwise if the input is moving the player left and the player is facing right...
-			else if (move < 0 && facingRight)
-            {
+			{
 				// ... flip the player
 				Flip();
-            }    
-        }
-    }
-
-    public void OnJump(InputAction.CallbackContext context)
-    {
-        Debug.Log("OnJump called");
-
-        if (context.started)
-        {
-			if (!jumping && grounded)
-			{
-				rigidbody.velocity = Vector2.zero;  // Set player's velocity to zero, stop moving when jump is pressed
 			}
-			
-            currentJumpForce = minJumpForce;
-            jumping = true;
-        }
-        else if (context.canceled)
-        {
-            Jump();
-        }
-    }
-
-    private void Jump()
-    {
-        jumping = false;
-        grounded = true;
-
-		// If the player is on the ground and jumps...
-		if (grounded)
-        {
-			// ... add vertical force to the player
-			rigidbody.AddForce(new Vector2(0f, currentJumpForce), ForceMode2D.Impulse);
-            currentJumpForce = minJumpForce;
+			// Otherwise if the input is moving the player left and the player is facing right...
+			else if (move < 0 && facingRight)
+			{
+				// ... flip the player
+				Flip();
+			}
 		}
 	}
 
-    private void Flip()
-    {
-        // Switch the way the player is facing
-        facingRight = !facingRight;
+	public void OnJump(InputAction.CallbackContext context)
+	{
 
-        // Multiply the player's lcoal scale by -1
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
-    }
+		Debug.Log("OnJump called");
+
+		if (context.started)
+		{
+			currentJumpForce = minJumpForce;
+			jumping = true;
+			grounded = false;   // grounded is false so that player cannot move in air
+
+			// if (!jumping && grounded)
+			// {
+			//	  rigidbody.velocity = Vector2.zero;  // Set player's velocity to zero, stop moving when jump is pressed
+			// }
+		}
+		else if (context.canceled)
+		{
+			Jump();
+		}
+	}
+
+	private void Jump()
+	{
+		jumping = false;
+		grounded = true;
+
+		// If the player is on the ground and jumps...
+		if (grounded)
+		{
+			// ... add vertical force to the player
+			rigidbody.AddForce(new Vector2(0f, currentJumpForce), ForceMode2D.Impulse);
+			currentJumpForce = minJumpForce;
+		}
+	}
+
+	private void Flip()
+	{
+		// Switch the way the player is facing
+		facingRight = !facingRight;
+
+		// Multiply the player's lcoal scale by -1
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
 }
